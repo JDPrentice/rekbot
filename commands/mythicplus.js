e7f0c923b01f71f22d125c296f7fb4387c4b1e12
JDPrentice <jprentice@protonmail.com>
2020-09-01 14:48:14 +0100

    small changes to mythicplus

--- commands/mythicplus.js
+++ commands/mythicplus.js
@@ -3,15 +3,21 @@ module.exports = {
     description: "pulls the raider.io API to show this week's M+ affixes",
     execute(message, args){
 
-    async () => {   
+    try {    
+      
         let url = 'https://raider.io/api/v1/mythic-plus/affixes?region=eu&locale=en';
-    
-        const response = await fetch(url);
-        const affix = await response.json();
+
+        const response = fetch(url);
+        const affix = response.json();
  
         console.log("mythicplus should have fired");
-        message.channel.send(affix);
-    } 
+        message.channel.send("mythicplus should have fired");
+     
+         }
 
-    }
+    catch (error){
+        console.error(error);
+		message.reply("The Mythic Plus command has failed");
+                }     
+}
 }
