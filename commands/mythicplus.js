4aeb58f2118eb811a542fb28d0eddde551d228b0
JDPrentice <jprentice@protonmail.com>
2020-09-01 19:40:55 +0100

    mythicplus command working

--- commands/mythicplus.js
+++ commands/mythicplus.js
@@ -1,29 +1,34 @@
 module.exports = {
     name: 'mythicplus',
     description: "pulls the raider.io API to show this week's M+ affixes",
-    execute(message, args){
+    execute(message, args) {
 
         const fetch = require('node-fetch');
         let url = "https://raider.io/api/v1/mythic-plus/affixes?region=eu&locale=en";
 
-    try {    
-
         fetch(url)
-        .then(response => { 
+            .then(response => {
+                console.log("mythicplus should have fired");
+                console.log("API Status " + response.statusText + " " + response.url);
 
-        console.log("mythicplus should have fired");
-        console.log("API Status " + response.statusText + " " + response.url);
+                response.json().then(data => {
+                    //message.channel.send("mythic plus affixes for this week are: " + data.title + "\n" + data.affix_details);
 
-        var affixData = response.json(); 
+                    data.affix_details.forEach(element => {
+                        message.channel.send({"embed": {
+                            "title": element.name,
+                            "url": element.wowhead_url,
+                            "description": element.description
+                        }})
+                    });
+                }).catch(error => {
+                    console.error(error);
+                });
+                
 
-        message.channel.send("mythic plus for this week should be " + affixData);
-        });
-        }
-         
-
-    catch (error){
-        console.error(error);
-		message.reply("The Mythic Plus command has failed");
-                }     
-}
+            }).catch(error => {
+                console.error(error);
+                message.reply("The Mythic Plus command has failed");
+            });
+    }
 }
